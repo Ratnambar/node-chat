30213fded28de629913a2597424f9e87d0711f38
Ratnambar <ratnambar123gupta@gmail.com>
2019-12-10 14:58:02 +0530

    hello commit

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const path=require('path');
+var express=require('express');
+const socketIO=require('socket.io');
+
+const publicPath=path.join(__dirname, '../public');
+const port=process.env.PORT || 3000;
+
+var app=express();
+
+app.use(express.static(publicPath));
+
+
+app.listen(port,()=>{
+  console.log(`Server is up on ${port}`);
+});
